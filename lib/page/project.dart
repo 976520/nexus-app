afe796858771e7e3e68af6ddee36ce07d5d38537
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-20 18:17:17 +0900

    Feat - ScrollView for project list, load when fully scrolled

--- lib/page/project.dart
+++ lib/page/project.dart
@@ -1,44 +1,49 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:nexus/components/project_preview.dart';
-import 'package:nexus/util/gap.dart';
-import 'package:nexus/util/interjector.dart';
 
-class ProjectPage extends StatelessWidget {
+class ProjectPage extends StatefulWidget {
   const ProjectPage({super.key});
 
   @override
+  State<ProjectPage> createState() => _ProjectPageState();
+}
+
+class _ProjectPageState extends State<ProjectPage> {
+  final List<int> _items = List<int>.generate(10, (index) => Random(index).nextInt(100));
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: interjector(
-          [
-            ProjectPreview(
-                project: Project(
-              name: 'Project Name',
-              description: 'Project Description',
-              stars: 100,
-            )),
-            ProjectPreview(
-                project: Project(
-              name: 'Project Name',
-              description: 'Project Description',
-              stars: 100,
-            )),
-            ProjectPreview(
-                project: Project(
-              name: 'Project Name',
-              description: 'Project Description',
-              stars: 100,
-            )),
-            ProjectPreview(
-                project: Project(
-              name: 'Project Name',
-              description: 'Project Description',
-              stars: 100,
-            )),
-          ],
-          const Gap(20),
+    final ScrollController scrollController = ScrollController();
+    scrollController.addListener(() {
+    if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        setState(() {
+          _items.addAll(List<int>.generate(10, (index) => Random(index).nextInt(100)));
+        });
+      }
+    });
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 20,
+        left: 40,
+        right: 40,
+      ),
+      child: ListView.separated(
+        itemBuilder: (context, index) {
+          return ProjectPreview(
+            project: Project(
+              name: 'Project $index',
+              description: 'This is a project description asdsadasdadddadsaddsdaasdasdadsadsadasasdsadadadadad',
+              stars: _items[index],
+            ),
+          );
+        },
+        separatorBuilder: (context, index) => const SizedBox(
+          height: 30,
         ),
+        itemCount: _items.length,
+        controller: scrollController,
       ),
     );
   }
