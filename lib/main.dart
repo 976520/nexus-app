eedd39ab2584b7b23360a7107e06872238ea37ba
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-24 10:01:52 +0900

    Feat : remove debug banner in dev mode

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Nexus App',
       theme: ThemeData(
         useMaterial3: true,
