d29dccba6ff9327f838f6a422db5a4eac20073e5
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-15 23:32:04 +0900

    Feat - login page init

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,1 +1,19 @@
-//TODO: make login with github
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Login Page'),
+        ElevatedButton(
+          onPressed: () {
+          },
+          child: const Text('Login with github'),
+        ),
+      ],
+    );
+  }
+}
